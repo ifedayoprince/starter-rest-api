0d7c67407edccd659981420c714d9323a744f8d9
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-02-16 21:19:49 +0100

    Updated logging in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import express from 'express';
 // import db from '@cyclic.sh/dynamodb';
 // import { router as core-router } from './core-router.js';
 // import { router as pine-router } from './pine-router.js';
@@ -9,13 +8,13 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use('/core', core-router);
-// app.use('/pine', pine-router);
+//app.use('/pine', pine-router);
 
 // Create new bearer token
 app.post("/super-user", (req, res) => {
 	const username = req.body.username;
     const password = req.body.password;
-  
+	console.log(password, username, req.body);
 	try {
 		if(password != process.env.SUPER_USER) throw new Error();
 		
