5af52389da121e5b32097465217499416e405f0a
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-02-18 08:02:00 +0100

    Updated preflight

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use('/pine', pineRouter);
 app.use('/core', coreRouter);
