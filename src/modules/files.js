454ea5bae8710a333e77018b3f59720be69eb1e4
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-02-19 21:18:18 +0100

    Updated versions

--- src/modules/files.js
+++ src/modules/files.js
@@ -1,12 +1,32 @@
 import { Router } from 'express';
 import { authenticateUser } from './../auth.js';
-import { S3, handleUploadMiddleware } from './setup.js';
+// import { S3, handleUploadMiddleware } from './setup.js';
+import { S3Client } from '@aws-sdk/client-s3';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+
 
 export const fRouter = new Router();
 
+const s3 = new S3Client()
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.CYCLIC_BUCKET_NAME,
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+
 // Accept maximum 5 files
 fRouter.post('/new', 
-	handleUploadMiddleware.array('files', 6),
+	upload.array('files', 6),
   (req, res) => {
   	console.log(req);
    if (req.files) {
